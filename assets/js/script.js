c29fa784f266919aea2768fc43adbede799c5da9
GiovanniDevs <damico.giovanni.90@gmail.com>
2026-01-21 14:34:50 +0000

    feat: add the canvas color picker similarly to the timer color picker
    the same way the color picker was made a new one was create to allow the user to select a color for the canvas as it will need to mach the application ini which will be embedded,  i.e. Notion.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,13 @@
 const colorInput = document.getElementById("favcolor");
-const colorButton = document.querySelector(".color-button");
+const colorButton = document.getElementById("color-button");
+
+const canvasInput = document.getElementById("favcolor-canvas");
+const canvasButton = document.getElementById("canvas-button");
 
 colorInput.addEventListener("input", () => {
   colorButton.style.backgroundColor = colorInput.value;
+});
+
+canvasInput.addEventListener("input", () => {
+  canvasButton.style.backgroundColor = canvasInput.value;
 });
