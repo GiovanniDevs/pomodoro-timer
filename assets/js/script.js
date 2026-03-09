55cb0a8a6bac50e7d5ee481f207fea70d38b5d52
GiovanniDevs <damico.giovanni.90@gmail.com>
2026-03-09 16:24:29 +0000

    add main functionalities of settings

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,23 +1,85 @@
-/* 
-Set the color of the visible button to the color picked via 
-the invisible picker, for the main timer
- */
+/* Color pickers */
 
-const colorInput = document.getElementById("favcolor");
-const colorButton = document.getElementById("color-button");
+var colorInput = document.getElementById("favcolor");
+var colorButton = document.getElementById("color-button");
+var canvasInput = document.getElementById("favcolor-canvas");
+var canvasButton = document.getElementById("canvas-button");
 
-colorInput.addEventListener("input", () => {
+function updateMainColor() {
   colorButton.style.backgroundColor = colorInput.value;
-});
+}
 
-/* 
-Set the color of the visible button to the color picked via 
-the invisible picker, for the canvas
- */
-
-const canvasInput = document.getElementById("favcolor-canvas");
-const canvasButton = document.getElementById("canvas-button");
-
-canvasInput.addEventListener("input", () => {
+function updateCanvasColor() {
   canvasButton.style.backgroundColor = canvasInput.value;
-});
+}
+
+if (colorInput && colorButton) {
+  colorInput.addEventListener("input", updateMainColor);
+}
+
+if (canvasInput && canvasButton) {
+  canvasInput.addEventListener("input", updateCanvasColor);
+}
+
+/* Sound settings */
+
+var soundSelect = document.getElementById("alert-sound");
+var soundToggle = document.getElementById("enable-alert");
+var volumeSlider = document.getElementById("alert-volume");
+var volumeValue = document.getElementById("volume-value");
+
+function updateSoundControls() {
+  var alertsEnabled = soundToggle.checked;
+  soundSelect.disabled = !alertsEnabled;
+  volumeSlider.disabled = !alertsEnabled;
+}
+
+function updateVolumeText() {
+  volumeValue.textContent = volumeSlider.value + "%";
+}
+
+if (soundSelect && soundToggle && volumeSlider && volumeValue) {
+  soundToggle.addEventListener("change", updateSoundControls);
+  volumeSlider.addEventListener("input", updateVolumeText);
+
+  updateSoundControls();
+  updateVolumeText();
+}
+
+/* Settings section show/hide */
+
+var tabButtons = document.querySelectorAll(".settings-nav-btn");
+var sections = document.querySelectorAll(".settings-panel");
+
+function showSection(sectionId) {
+  var i;
+
+  for (i = 0; i < sections.length; i++) {
+    if (sections[i].id === sectionId) {
+      sections[i].classList.add("is-active");
+    } else {
+      sections[i].classList.remove("is-active");
+    }
+  }
+
+  for (i = 0; i < tabButtons.length; i++) {
+    if (tabButtons[i].getAttribute("data-target") === sectionId) {
+      tabButtons[i].classList.add("is-active");
+    } else {
+      tabButtons[i].classList.remove("is-active");
+    }
+  }
+}
+
+function onTabClick(event) {
+  var selectedSectionId = event.currentTarget.getAttribute("data-target");
+  showSection(selectedSectionId);
+}
+
+if (tabButtons.length > 0 && sections.length > 0) {
+  var j;
+
+  for (j = 0; j < tabButtons.length; j++) {
+    tabButtons[j].addEventListener("click", onTabClick);
+  }
+}
